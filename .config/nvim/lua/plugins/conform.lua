268fa79fa2580bbc59bc560cae2a16e5ea782015
valdezdata <valdezdata@tuta.io>
2025-05-31 22:59:03 -0700

    update dots

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,61 +1,74 @@
-local conform = require "conform"
+local conform = require("conform")
 
-conform.setup {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Python: comprehensive formatting with import sorting and fixes
-    python = {
-      "ruff_format", -- Primary formatter
-      "ruff_fix", -- Code fixes
-    },
-    -- Web development
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
-    json = { "prettier" },
-    jsonc = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
+conform.setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Python: comprehensive formatting with import sorting and fixes
+		python = {
+			"ruff_format", -- Primary formatter
+			"ruff_fix", -- Code fixes
+		},
+		-- Web development
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		jsonc = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
 
-    -- Infrastructure and DevOps
-    terraform = {
-      "terraform_fmt",
-    },
-    dockerfile = { "prettier" },
+		-- Infrastructure and DevOps
+		terraform = {
+			"terraform_fmt",
+		},
+		dockerfile = { "prettier" },
 
-    -- Shell scripting
-    sh = { "shfmt" },
-    bash = { "shfmt" },
+		-- Shell scripting
+		sh = { "shfmt" },
+		bash = { "shfmt" },
 
-    -- Golang
-    go = {
-      "goimports", -- Handles imports
-      "gofmt", -- Standard formatting
-    },
+		-- Golang
+		go = {
+			"goimports", -- Handles imports
+			"gofmt", -- Standard formatting
+		},
 
-    -- SQL
-    sql = { "sql-formatter" },
-  },
+		-- SQL
+		sql = { "sql-formatter" },
+	},
 
-  -- Format options
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-    async = false,
-  },
+	-- -- Format options
+	-- format_on_save = {
+	--   timeout_ms = 500,
+	--   lsp_fallback = true,
+	--   async = false,
+	-- },
 
-  -- Notify on formatting errors
-  notify_on_error = true,
-}
+	-- Disable format on save for TypeScript and TypeScriptReact
+	format_on_save = function(bufnr)
+		local ft = vim.bo[bufnr].filetype
+		if ft == "typescript" or ft == "typescriptreact" then
+			return
+		end
+		return {
+			timeout_ms = 500,
+			lsp_fallback = true,
+			async = false,
+		}
+	end,
+
+	-- Notify on formatting errors
+	notify_on_error = true,
+})
 
 -- Key mappings for manual formatting
 vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-  conform.format {
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500,
-  }
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
 end, { desc = "Format file or range (in visual mode)" })
